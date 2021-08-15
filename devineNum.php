13d6ce9b668af25dff4f57feeeed41a62c0db212
moustii <jeremy.ibert@outlook.com>
2021-08-15 22:48:33 +0200

    features : afichage nombre d'essai

--- devineNum.php
+++ devineNum.php
@@ -1,31 +1,45 @@
 <?php
+// Pour pouvoir acceder à la variable glabale
+session_start();
 
-$alea = 5;
+if(!isset($_SESSION["aleatoire"])) {
+    $_SESSION["aleatoire"] = rand(1, 100);
+}
+
+if(!isset($_SESSION["essai"])) {
+    $_SESSION["essai"] = 0;
+}
+
+var_dump($_SESSION);
+
 $nombre;
 $status = "";
 
 
+// comparaison nombre saisie et nombre voulue
 if (isset($_GET["chiffre"])){
     $nombre = (int)($_GET["chiffre"]);
-
     switch($nombre){
-        case $nombre < $alea :
+        case $nombre < $_SESSION["aleatoire"] :
             $status .= "C'est plus";
-
+            $_SESSION["essai"]++;
             break;
-        case $nombre > $alea :
+        case $nombre > $_SESSION["aleatoire"] :
             $status .= "C'est moins";
+            $_SESSION["essai"]++;
             break;
-        case $nombre === $alea :
+        case $nombre === $_SESSION["aleatoire"] :
             $status .= "<mark> Well done ! </mark>";
+            session_destroy();
             break;
         default :
             echo "Erreur de saisie";
     }
+   
 }
 
-?>
 
+?>
 
 <!--
     ---------------------------------------------
@@ -39,10 +53,6 @@ if (isset($_GET["chiffre"])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <script>
-        var button = document.querySelector('.btn')
-        button.addEventListener('click', e => console.log('click'))
-    </script>
 </head>
 <body style="background-color: #ddd;">
 
@@ -54,25 +64,15 @@ if (isset($_GET["chiffre"])){
 <div>
     <form action="" method="GET" style="text-align: center;">
                 <br>  
-                    <input type="number" name="chiffre" placeholder="Entre un nombre entre 1 et 100" autofocus min="1" max="100" style="border: 1px solid black; border-radius:5px; width: 17%; height: 7vh;">
-                    <br><br>
-                    <button class="btn">Aller GO</button >  
+                <input type="number" name="chiffre" placeholder="Entre un nombre entre 1 et 100" autofocus min="1" max="100" style="border: 1px solid black; border-radius:5px; width: 17%; height: 7vh;" value="salut">
+                <br><br>
+                <button>Aller GO</button >  
     </form>
     <p style="text-align: center; color: 1d1d1d"><?php if(isset($_GET["chiffre"])){ echo $_GET["chiffre"] . " : " . $status;} ?></p>
+    <p style="text-align: center; color: 1d1d1d"><?= "nombre d'essai : " . $_SESSION["essai"] ?></p>
     
 
 </div>
-
-
-    
-
-
-    
-
-
-
-
-
 
 
 </body>
